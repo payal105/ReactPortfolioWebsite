104f9cc32007ee2770a21663171e9f0d0723750f
Payal Adhikary <payaladhikary2000@gmail.com>
2023-03-04 13:04:48 +0530

    Update Awards.jsx

--- src/components/certificates&awards/Awards.jsx
+++ src/components/certificates&awards/Awards.jsx
@@ -1,5 +1,6 @@
 import Cert1 from "../../assets/cert-1.jpg"
 import Cert2 from "../../assets/cert-2.jpg"
+import Cert9 from "../../assets/cert-9.jpg"
 import Oth1 from "../../assets/oth-1.jpg"
 import Oth4 from "../../assets/oth-4.jpg"
 import IT1 from "../../assets/it-1.jpg"
@@ -9,7 +10,7 @@ import Boot1 from "../../assets/boot-1.jpg"
 
 const Menu = [
     {
-        id: 1,
+        id: 9,
         image: Cert1,
         title: "Topper in Cloud Computing from NPTEL",
         category: "Courses",
@@ -63,6 +64,13 @@ const Menu = [
         title: "Course on Enhancing Softskills & Personality from NPTEL",
         category: "Others",
         link: Cert2
+    },
+     {
+        id: 1,
+        image: Cert9,
+        title: "PHP Course Completion Certificate from Great Learning",
+        category: "Others",
+        link: Cert9
     },
     
 ]
